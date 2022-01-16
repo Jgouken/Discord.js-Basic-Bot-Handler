ca41df152922c5d32d26a9a7617845d7a2095b9b
Jgouken <65487563+Jgouken@users.noreply.github.com>
2022-01-16 14:43:07 -0800

    Fix for v13

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
 module.exports = {
     Discord: Discord, 
